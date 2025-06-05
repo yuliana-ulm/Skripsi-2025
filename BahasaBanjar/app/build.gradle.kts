4078d6b2cf7bb69c0fb81532f6466cc67031dd25
yuliana-ulm <yana84594@gmail.com>
2025-06-06 06:09:48 +0800

    update android

--- BahasaBanjar/app/build.gradle.kts
+++ BahasaBanjar/app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation(libs.appcompat)
+    implementation(libs.androidx.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
