08b19a1722fbd903bd7c8e38f4e8ce0f7a6ce69d
yuliana-ulm <yana84594@gmail.com>
2025-06-10 23:21:52 +0800

    baru lagi apknya

--- BahasaBanjar/app/build.gradle.kts
+++ BahasaBanjar/app/build.gradle.kts
@@ -69,6 +69,11 @@ dependencies {
     implementation(libs.firebase.crashlytics.buildtools)
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.firebase.inappmessaging)
+    implementation(libs.firebase.storage)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
